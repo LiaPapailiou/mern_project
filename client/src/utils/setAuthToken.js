9b9bbf15f20f4add1b0308f3915ad3bcfbb04e1c
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-05-15 13:07:32 +0200

    load user and set auth token

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const setAuthToken = token => {
+    if (!token) {
+        delete axios.defaults.headers.common['x-auth-token'];
+    }
+    axios.defaults.headers.common['x-auth-token'] = token;
+}
+
+export default setAuthToken
