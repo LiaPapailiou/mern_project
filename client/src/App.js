a40cb2878060bda4c35a93ca3ad5cec8000be03c
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-05-18 14:32:08 +0200

    add education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
 import CreateProfile from './components/profile-forms/CreateProfile'
+import AddExperience from './components/profile-forms/AddExperience'
+import AddEducation from './components/profile-forms/AddEducation'
 import EditProfile from './components/profile-forms/EditProfile'
 import Alert from './components/layout/Alert'
 import { Provider } from 'react-redux'
@@ -37,6 +39,8 @@ const App = () => {
                             <PrivateRoute exact path="/dashboard" component={Dashboard} />
                             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
                             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+                            <PrivateRoute exact path="/add-education" component={AddEducation} />
+                            <PrivateRoute exact path="/add-experience" component={AddExperience} />
                         </Switch>
                     </section>
                 </Fragment>
