21436d39a23bc3a39e068f6a3c2ccbe5af08bee1
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-05-20 22:30:46 +0200

    delete post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,9 +2,15 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
+import { addLike, removeLike, deletePost } from '../../actions/posts'
 import PropTypes from 'prop-types'
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date } }) => {
+const PostItem = ({
+    addLike,
+    removeLike,
+    deletePost,
+    auth,
+    post: { _id, text, name, avatar, user, likes, comments, date } }) => {
     return (
         <div className="post bg-white p-1 my-1">
             <div>
@@ -24,13 +30,13 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
                 <p className="post-date">
                     Posted on <Moment format='DD/MM/YYYY'>{date}</Moment>
                 </p>
-                <button type="button" className="btn btn-light">
+                <button onClick={(e) => addLike(_id)} type="button" className="btn btn-light">
                     <i className="fas fa-thumbs-up"></i>{' '}
                     {likes.length > 0 && (
                         <span>{likes.length}</span>
                     )}
                 </button>
-                <button type="button" className="btn btn-light">
+                <button onClick={(e) => removeLike(_id)} type="button" className="btn btn-light">
                     <i className="fas fa-thumbs-down"></i>
                 </button>
                 <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -39,7 +45,7 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
                     )}
                 </Link>
                 {!auth.loading && user === auth.user._id && (
-                    <button type="button" className="btn btn-danger">
+                    <button onClick={(e) => deletePost(_id)} type="button" className="btn btn-danger">
                         <i className="fas fa-times"></i>
                     </button>
                 )}
@@ -51,8 +57,11 @@ const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 }
 const mapStateToProps = (state) => ({
     auth: state.auth
 })
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem)
