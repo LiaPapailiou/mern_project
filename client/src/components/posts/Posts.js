bef4829bd64f14206c74755fbaa99c01ed862d8b
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-05-21 12:15:35 +0200

    add comments, posts and single post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getPosts } from '../../actions/posts'
 import Spinner from '../layout/Spinner'
 import PostItem from './PostItem'
+import PostForm from './PostForm'
 
 const Posts = ({ getPosts, posts: { posts, loading } }) => {
     useEffect(() => {
@@ -15,6 +16,7 @@ const Posts = ({ getPosts, posts: { posts, loading } }) => {
             <p className="lead">
                 <i className="fas fa-user"></i> Welcome to the Community
             </p>
+            <PostForm />
             <div className="posts">
                 {posts.map((post) => (
                     <PostItem key={post._id} post={post} />
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,13 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from '../actions/types'
+import {
+    GET_POSTS,
+    GET_POST,
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
+} from '../actions/types'
 const initialState = {
     posts: [],
     post: null,
@@ -13,6 +22,18 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 posts: payload,
+                loading: false
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false
+            }
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [payload, ...state.posts],
                 loading: false
             }
         case DELETE_POST:
@@ -32,6 +53,21 @@ export default function (state = initialState, action) {
                 ...state,
                 posts: state.posts.map((post) => post._id === payload.id ? { ...post, likes: payload.likes } : post),
                 loading: false
+            }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false
+            }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter((comment) => comment._id !== payload),
+                    loading: false
+                }
             }
         default:
             return state
